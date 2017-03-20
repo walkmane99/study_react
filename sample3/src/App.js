436c3e644f3d10b7a218e43b59b9ea8325a45bb2
村上浩二 <murakami.k@crea-coco.com>
2017-03-21 00:39:49 +0900

    sample3

--- sample3/src/App.js
+++ sample3/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Controller from './Controller';
 
 
 
@@ -8,31 +7,29 @@ class App extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            count:0
-        }
-        this.handleUp = this.handleCountup.bind(this);
-        this.handleDown = this.handleCountdown.bind(this);
     }
-  handleCountup() {
-      this.setState({count:this.state.count +1 });
-  }
 
-  handleCountdown() {
-      this.setState({count:this.state.count -1 });
-  }
   render() {
     return (
       <div className="App">
-        <button onClick={this.handleUp} >
-          カウントアップ
-        </button>
-        <button onClick={this.handleDown} >
-          カウントダウン
-        </button>
-        <p className="App-intro">
-            {this.state.count}
-        </p>
+        <Controller >
+            {((c) => {
+                return (
+                <div>
+                    <button onClick={c.countup} >
+                        カウントアップ
+                    </button>
+                    <button onClick={c.countdown} >
+                        カウントダウン
+                    </button>
+                    <p className="App-intro">
+                        {c.count}
+                    </p>
+                </div>
+                )
+            })}
+        </Controller>
+
       </div>
     );
   }
--- sample3/src/Controller.js
+++ sample3/src/Controller.js
@@ -1,3 +1,28 @@
-/**
- * Created by koji on 2017/03/20.
- */
+import React, { Component } from 'react';
+
+
+
+class Controller extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            count:0
+        }
+        this.handleUp = this.handleCountup.bind(this);
+        this.handleDown = this.handleCountdown.bind(this);
+    }
+    handleCountup() {
+        this.setState({count:this.state.count +1 });
+    }
+
+    handleCountdown() {
+        this.setState({count:this.state.count -1 });
+    }
+
+    render() {
+        return this.props.children({count:this.state.count, countup:this.handleUp, countdown:this.handleDown});
+    }
+}
+
+export default Controller;
